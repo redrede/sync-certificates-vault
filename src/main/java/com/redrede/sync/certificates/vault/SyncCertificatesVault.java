dedcf953b7bc65a29c40a02aba09ce1a7176edb1
Marcelo Canhamero <marcelo@redrede.net>
2023-03-15 21:17:39 -0300

    add mode sync vault to file

--- src/main/java/com/redrede/sync/certificates/vault/SyncCertificatesVault.java
+++ src/main/java/com/redrede/sync/certificates/vault/SyncCertificatesVault.java
@@ -3,7 +3,10 @@ package com.redrede.sync.certificates.vault;
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -21,7 +24,7 @@ import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
 
 /**
- * Sync certificates to Vault
+ * Sync certificates to Vault or Vault to certificates
  *
  * @author marcelo
  */
@@ -30,37 +33,60 @@ public class SyncCertificatesVault {
     public static void main(String[] args) {
         try {
             switch (args.length) {
-                case 5:
-                    System.out.println("Parameters");
-                    System.out.println("certificates-path: " + args[0]);
-                    System.out.println("certificates-extension: " + args[1]);
-                    System.out.println("vault-address " + args[2]);
-                    // hide token
-                    System.out.println("vault-token: " + args[3].replaceAll("[A-Za-z0-9]", "*"));
-                    System.out.println("vault-path: " + args[4]);
-                    syncFiles(args[0], args[1], args[2], args[3], args[4], null);
-                    break;
                 case 6:
                     System.out.println("Parameters");
-                    System.out.println("certificates-path: " + args[0]);
-                    System.out.println("certificates-extension: " + args[1]);
-                    System.out.println("vault-address " + args[2]);
+                    System.out.println("mode: " + args[0]); //file2Vault | vault2File
+                    System.out.println("certificates-path: " + args[1]);
+                    System.out.println("certificates-extension: " + args[2]);
+                    System.out.println("vault-address " + args[3]);
                     // hide token
-                    System.out.println("vault-token: " + args[3].replaceAll("[A-Za-z0-9]", "*"));
-                    System.out.println("vault-path: " + args[4]);
-                    System.out.println("dynamic-path: " + args[5]);
-                    syncFiles(args[0], args[1], args[2], args[3], args[4], args[5]);
+                    System.out.println("vault-token: " + args[4].replaceAll("[A-Za-z0-9]", "*"));
+                    System.out.println("vault-path: " + args[5]);
+                    if (args[0].equalsIgnoreCase("file2Vault")) {
+                        syncFiles(args[1], args[2], args[3], args[4], args[5], null);
+                    } else if (args[0].equalsIgnoreCase("vault2File")) {
+                        syncVault(args[1], args[2], args[3], args[4], args[5]);
+                    } else {
+                        System.err.println("Error:");
+                        System.err.println("Enter mode parameter correctly");
+                        System.err.println(" file2Vault -> sync file to vault");
+                        System.err.println(" vault2File -> sync vault to file");
+                        System.out.println("java -jar sync-certificates-vault.jar <mode> <certificates-path> <certificates-extension> <vault-address> <vault-token>  <vault-path> [<dynamic-path>]");
+                        System.out.println("Example: ");
+                        System.out.println("java -jar sync-certificates-vault.jar \"file2Vault\" \"/etc/letsencrypt/live\" \"pem\" \"http://127.0.0.1:1234\" \"myroot\" \"secrets/certificates\"  \"/etc/letsencrypt/dynamic\"");
+                    }
+                    break;
+                case 7:
+                    System.out.println("Parameters");
+                    System.out.println("mode: " + args[0]); //file2Vault | vault2File
+                    System.out.println("certificates-path: " + args[1]);
+                    System.out.println("certificates-extension: " + args[2]);
+                    System.out.println("vault-address " + args[3]);
+                    // hide token
+                    System.out.println("vault-token: " + args[4].replaceAll("[A-Za-z0-9]", "*"));
+                    System.out.println("vault-path: " + args[5]);
+                    System.out.println("dynamic-path: " + args[6]);
+                    if (args[0].equalsIgnoreCase("file2Vault")) {
+                        syncFiles(args[1], args[2], args[3], args[4], args[5], args[6]);
+                    } else {
+                        System.err.println("Error:");
+                        System.err.println("Enter mode parameter correctly");
+                        System.err.println(" vault2File does not support \"dynamic-path\"");
+                        System.out.println("java -jar sync-certificates-vault.jar <mode> <certificates-path> <certificates-extension> <vault-address> <vault-token>  <vault-path> [<dynamic-path>]");
+                        System.out.println("Example: ");
+                        System.out.println("java -jar sync-certificates-vault.jar \"file2Vault\" \"/etc/letsencrypt/live\" \"pem\" \"http://127.0.0.1:1234\" \"myroot\" \"secrets/certificates\"  \"/etc/letsencrypt/dynamic\"");
+                    }
                     break;
                 default:
                     System.err.println("Error:");
                     System.err.println("Enter the parameters");
-                    System.out.println("java -jar sync-certificates-vault.jar <certificates-path> <certificates-extension> <vault-address> <vault-token>  <vault-path> [<dynamic-path>]");
+                    System.out.println("java -jar sync-certificates-vault.jar <mode> <certificates-path> <certificates-extension> <vault-address> <vault-token>  <vault-path> [<dynamic-path>]");
                     System.out.println("Example: ");
-                    System.out.println("java -jar sync-certificates-vault.jar \"/etc/letsencrypt/live\" \"pem\" \"http://127.0.0.1:1234\" \"myroot\" \"secrets/certificates\"  \"/etc/letsencrypt/dynamic\"");
+                    System.out.println("java -jar sync-certificates-vault.jar \"file2Vault\" \"/etc/letsencrypt/live\" \"pem\" \"http://127.0.0.1:1234\" \"myroot\" \"secrets/certificates\"  \"/etc/letsencrypt/dynamic\"");
                     break;
             }
         } catch (IOException e) {
-            System.out.println("X - " + e.getMessage());
+            System.err.println("X - " + e.getMessage());
         }
     }
 
@@ -124,10 +150,36 @@ public class SyncCertificatesVault {
 
         try {
             monitor.start();
-            System.out.println("Start Certificate Sync");
-
+            System.out.println("Start Certificate Sync Files to Vault");
         } catch (IOException e) {
             System.err.println("X - " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.err.println("X - " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("X - " + e.getMessage());
+        }
+    }
+
+    /**
+     * Monitors change in Vault
+     *
+     * @param certificatesPath
+     * @param certificatesExtension
+     * @param vaultAddress
+     * @param vaultToken
+     * @param vaultPath
+     * @throws IOException
+     */
+    public static void syncVault(String certificatesPath, String certificatesExtension, String vaultAddress, String vaultToken, String vaultPath) throws IOException {
+        try {
+            System.out.println("Start Certificate Sync Vault to files - every 10 minutes");
+            while (true) {
+                List<String> list = getVaultList(vaultAddress, vaultToken, vaultPath);
+                for (String domain : list) {
+                    getCertificate(vaultAddress, vaultToken, vaultPath, domain, certificatesPath, certificatesExtension);
+                }
+                Thread.sleep(600000);
+            }
         } catch (InterruptedException e) {
             System.err.println("X - " + e.getMessage());
         } catch (Exception e) {
@@ -171,25 +223,7 @@ public class SyncCertificatesVault {
     public static void sendVault(String vaultAddress, String vaultToken, String vaultPath, String domain, File certificate, String dynamicPath, boolean create) {
         try {
             Vault vault;
-            if (vaultToken.startsWith("approle")) {
-                StringTokenizer approle = new StringTokenizer(vaultToken, ":");
-                String path = approle.nextToken();
-                String roleId = approle.nextToken();
-                String secretId = approle.nextToken();
-                String token = new Vault(new VaultConfig().address(vaultAddress).build(), 2).auth().loginByAppRole(path, roleId, secretId).getAuthClientToken();
-                VaultConfig config = new VaultConfig()
-                        .address(vaultAddress)
-                        .token(token)
-                        .build();
-                vault = new Vault(config, 2);
-            } else {
-                VaultConfig config = new VaultConfig()
-                        .address(vaultAddress)
-                        .token(vaultToken)
-                        .build();
-                vault = new Vault(config, 2);
-            }
-
+            vault = getVault(vaultToken, vaultAddress);
             if (create) {
                 String content = FileUtils.readFileToString(certificate, StandardCharsets.UTF_8);
                 final Map<String, Object> secrets = new HashMap<>();
@@ -211,5 +245,126 @@ public class SyncCertificatesVault {
             System.err.println("X - " + ex.getMessage());
         }
 
+    }
+
+    /**
+     * Returns access to Vault
+     *
+     * @param vaultToken
+     * @param vaultAddress
+     * @return
+     * @throws VaultException
+     */
+    private static Vault getVault(String vaultToken, String vaultAddress) throws VaultException {
+        Vault vault;
+        if (vaultToken.startsWith("approle")) {
+            StringTokenizer approle = new StringTokenizer(vaultToken, ":");
+            String path = approle.nextToken();
+            String roleId = approle.nextToken();
+            String secretId = approle.nextToken();
+            String token = new Vault(new VaultConfig().address(vaultAddress).build(), 2).auth().loginByAppRole(path, roleId, secretId).getAuthClientToken();
+            VaultConfig config = new VaultConfig()
+                    .address(vaultAddress)
+                    .token(token)
+                    .build();
+            vault = new Vault(config, 2);
+        } else {
+            VaultConfig config = new VaultConfig()
+                    .address(vaultAddress)
+                    .token(vaultToken)
+                    .build();
+            vault = new Vault(config, 2);
+        }
+        return vault;
+    }
+
+    /**
+     * Get vault list
+     *
+     * @param vaultAddress
+     * @param vaultToken
+     * @param vaultPath
+     * @return
+     */
+    private static List<String> getVaultList(String vaultAddress, String vaultToken, String vaultPath) {
+        List<String> list = null;
+        Vault vault;
+        try {
+            vault = getVault(vaultToken, vaultAddress);
+            list = vault.logical().list(vaultPath).getListData();
+        } catch (VaultException ex) {
+            System.err.println("X - " + ex.getMessage());
+        }
+        return list;
+    }
+
+    /**
+     * Get certificate
+     *
+     * @param vaultAddress
+     * @param vaultToken
+     * @param vaultPath
+     * @return
+     */
+    private static void getCertificate(String vaultAddress, String vaultToken, String vaultPath, String domain, String certificatesPath, String certificatesExtension) {
+        Vault vault;
+        try {
+            vault = getVault(vaultToken, vaultAddress);
+            Map<String, String> currentSecrets = vault.logical().read(vaultPath + "/" + domain).getData();
+            for (Map.Entry<String, String> secret : currentSecrets.entrySet()) {
+                if (secret.getKey().endsWith(certificatesExtension)) {
+                    File folder = new File(certificatesPath + "/" + domain);
+                    if (!folder.exists()) {
+                        folder.mkdirs();
+                    }
+                    if (!readCert(certificatesPath, domain, secret.getKey()).equals(secret.getValue())) {
+                        saveCert(secret.getValue(), certificatesPath, domain, secret.getKey());
+                    }
+                } else {
+                    System.out.println("X - File creation skipped: " + secret.getKey());
+                }
+            }
+        } catch (VaultException ex) {
+            System.err.println("X - " + ex.getMessage());
+        }
+
+    }
+
+    /**
+     * Write the certificate to disk
+     *
+     * @param content
+     * @param certificatesPath
+     * @param domain
+     * @param fileName
+     */
+    private static void saveCert(String content, String certificatesPath, String domain, String fileName) {
+        try (FileWriter writer = new FileWriter(certificatesPath + "/" + domain + "/" + fileName)) {
+            writer.write(content);
+            System.out.println("V - Saved file: " + fileName + " -> " + certificatesPath + "/" + domain);
+        } catch (IOException ex) {
+            System.err.println("X - " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Write the certificate to disk
+     *
+     * @param content
+     * @param certificatesPath
+     * @param domain
+     * @param fileName
+     */
+    private static String readCert(String certificatesPath, String domain, String fileName) {
+        String cert = "";
+        try {
+            File file = new File(certificatesPath + "/" + domain + "/" + fileName);
+            if (file.exists()) {                
+                cert = new String(Files.readAllBytes(Paths.get(certificatesPath + "/" + domain + "/" + fileName)));
+            }
+        } catch (IOException ex) {
+            System.err.println("X - " + ex.getMessage());
+        }
+        return cert;
     }
 }
